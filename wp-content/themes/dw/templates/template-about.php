70a4e32b5b16a30aa6cc01b260387be9abe5204f
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2025-02-14 10:57:51 +0100

    Adding contact template

---
+++ wp-content/themes/dw/templates/template-about.php
@@ -0,0 +1,19 @@
+<?php /* Template Name: Page "À propos" */?>
+<?php get_header(); ?>
+
+<aside>
+    <h2>À propos de moi</h2>
+</aside>
+<!--On ouvre "la boucle" ("The Loop"), la structure de contrôle de contenu propre à Wordpress-->
+<?php if (have_posts()): while (have_posts()): the_post(); ?>
+
+
+    <div><?= get_the_content(); ?></div>
+
+
+    <!--On ferme "la boucle" ("The Loop")-->
+<?php endwhile; else : ?>
+    <p>La page est vide !</p> <!--Si jamais il n'y a rien à afficher-->
+<?php endif; ?>
+
+<?php get_footer(); ?>
