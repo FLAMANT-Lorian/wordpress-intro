1e7efc7758f4f470cafa51f21a58e95261f83450
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2025-02-14 11:55:34 +0100

    Added excerpt & thumbnail

--- wp-content/themes/dw/single-recipe.php
+++ wp-content/themes/dw/single-recipe.php
@@ -1,19 +1,40 @@
 <?php get_header(); ?>
 
-<style>
-    .recipe{
-        display: flex;
-        flex-direction: row-reverse;
-        justify-content: space-between;
-    }
+    <style>
+        .recipe {
+            display: flex;
+            flex-direction: row-reverse;
+            justify-content: space-between;
+        }
 
-    .recipe__ingredients{
-        width: 320px;
-        padding: 20px;
-        background-color: #F1F1F1;
-    }
+        .recipe__ingredients {
+            width: 320px;
+            padding: 20px;
+            background-color: #F1F1F1;
+            display: flex;
+            flex-direction: column-reverse;
+        }
 
-</style>
+        .recipe_fig{ /*Image qui s'adapte en fonction de la taille du carré (container)*/
+            display: block;
+            position: relative;
+            width: 100%;
+            height: 0;
+            padding-top: 100%;
+            margin: 0;
+        }
+
+        .recipe_img{
+            display: block;
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+
+    </style>
 
 
     <!--On ouvre "la boucle" ("The Loop"), la structure de contrôle de contenu propre à Wordpress-->
@@ -22,10 +43,19 @@
 
     <h2><?= get_the_title(); ?></h2>
 
+    <p><?= get_the_excerpt(); ?></p>
+
     <div class="recipe">
         <aside class="recipe__ingredients">
-            <h3>Ingrédients</h3>
-            <p>À compléter&nbsp;!</p>
+            <div>
+                <h3>Ingrédients</h3>
+                <p>À compléter&nbsp;!</p>
+                <figure class="recipe_fig">
+                    <?= get_the_post_thumbnail(size: 'medium', attr: [
+                            'class' => 'recipe_img', // Ajouter un class à l'image
+                    ]); ?>
+                </figure>
+            </div>
         </aside>
 
         <section class="recipe__steps">
