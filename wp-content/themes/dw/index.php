96e56c33917e1a55d62f4123993c335125cde16a
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2025-02-14 09:32:18 +0100

    Added first iteration example of "The Loop"

--- wp-content/themes/dw/index.php
+++ wp-content/themes/dw/index.php
@@ -9,7 +9,24 @@
     <title><?= wp_title('·', 'false', 'right') . get_bloginfo('name'); ?></title>
 </head>
 <body>
-    <h1><?= get_bloginfo('name'); ?></h1>
-    <p><?= get_bloginfo('description'); ?></p>
+    <header>
+        <h1><?= get_bloginfo('name'); ?></h1>
+        <p><?= get_bloginfo('description'); ?></p>
+    </header>
+<main>
+    <!--On ouvre "la boucle" ("The Loop"), la structure de contrôle de contenu propre à Wordpress-->
+    <?php if (have_posts()): while (have_posts()): the_post(); ?>
+
+
+
+        <h2><?= get_the_title(); ?></h2>
+        <div><?= get_the_content(); ?></div>
+    
+
+        <!--On ferme "la boucle" ("The Loop")-->
+    <?php endwhile; else : ?>
+        <p>La page est vide !</p> <!--Si jamais il n'y a rien à afficher-->
+    <?php endif; ?>
+</main>
 </body>
 </html>
